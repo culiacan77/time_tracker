2f3e96da28d9f0da50374547ed8cac7a27568e01
Thomas Fessler <culiacan77@users.noreply.github.com>
2026-01-11 00:02:55 +0100

    ajout clockwheel hour et clockwheel day

--- main/main.cpp
+++ main/main.cpp
@@ -5,7 +5,7 @@
 #include <inttypes.h>
 
 #include "clockwheel.hpp"
-#include "driver/gpio.h"  //permet de paramettrer les gpio en tant qu'input/output, pupllup/pulldown
+#include "driver/gpio.h" //permet de paramettrer les gpio en tant qu'input/output, pupllup/pulldown
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_timer.h"
@@ -14,25 +14,39 @@
 #include "stepper-motor-4p.hpp"
 
 // tag pour les messages de debug
-const char* kTag = "main";
+const char *kTag = "main";
 
 constexpr gpio_num_t kMinuteMotorPin1 = GPIO_NUM_1;
 constexpr gpio_num_t kMinuteMotorPin2 = GPIO_NUM_5;
 constexpr gpio_num_t kMinuteMotorPin3 = GPIO_NUM_6;
 constexpr gpio_num_t kMinuteMotorPin4 = GPIO_NUM_7;
 
+constexpr gpio_num_t kHourMotorPin1 = GPIO_NUM_8;
+constexpr gpio_num_t kHourMotorPin2 = GPIO_NUM_10;
+constexpr gpio_num_t kHourMotorPin3 = GPIO_NUM_9;
+constexpr gpio_num_t kHourMotorPin4 = GPIO_NUM_14;
+
+constexpr gpio_num_t kDayMotorPin1 = GPIO_NUM_11;
+constexpr gpio_num_t kDayMotorPin2 = GPIO_NUM_13;
+constexpr gpio_num_t kDayMotorPin3 = GPIO_NUM_12;
+constexpr gpio_num_t kDayMotorPin4 = GPIO_NUM_4;
+
 constexpr gpio_num_t kStatusLedPin = GPIO_NUM_47;
 
-constexpr gpio_num_t kMinuteSwitchUpPin   = GPIO_NUM_21;
+constexpr gpio_num_t kMinuteSwitchUpPin = GPIO_NUM_21;
 constexpr gpio_num_t kMinuteSwitchDownPin = GPIO_NUM_17;
+constexpr gpio_num_t kHourSwitchUpPin = GPIO_NUM_34;
+constexpr gpio_num_t kHourSwitchDownPin = GPIO_NUM_35;
+constexpr gpio_num_t kDaySwitchUpPin = GPIO_NUM_33;
+constexpr gpio_num_t kDaySwitchDownPin = GPIO_NUM_36;
 
 constexpr int kLedStripRmtResHz = (10 * 1000 * 1000);
-constexpr int kLoopDelayMs      = 12;  // delay time in ms for the main loop
-constexpr int kMotorStepNumber  = 2048;
+constexpr int kLoopDelayMs = 12; // delay time in ms for the main loop
+constexpr int kMotorStepNumber = 2048;
 
-constexpr int64_t kMotorMinuteFrequency = 60e6;  // 60 * 10^6
-constexpr int64_t kMotorHourFrequency   = kMotorMinuteFrequency * 60;
-constexpr int64_t kMotorDayFrequency    = kMotorHourFrequency * 24;
+constexpr int64_t kMotorMinuteFrequency = 60e6; // 60 * 10^6
+constexpr int64_t kMotorHourFrequency = kMotorMinuteFrequency * 60;
+constexpr int64_t kMotorDayFrequency = kMotorHourFrequency * 24;
 
 //  attention, l'ordre des arguments des pin de moteurs doit être 1 3 2 4
 //(le 3 et le 2 sont inversés)
@@ -42,33 +56,48 @@ void app_main(void);
 }
 
 void app_main(void) {
-    led_strip_config_t strip_config     = {};
-    strip_config.strip_gpio_num         = kStatusLedPin;
-    strip_config.max_leds               = 1;
-    strip_config.led_model              = LED_MODEL_WS2812;
-    strip_config.color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_RGB;
+  led_strip_config_t strip_config = {};
+  strip_config.strip_gpio_num = kStatusLedPin;
+  strip_config.max_leds = 1;
+  strip_config.led_model = LED_MODEL_WS2812;
+  strip_config.color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_RGB;
 
-    led_strip_rmt_config_t rmt_config = {};
-    rmt_config.clk_src                = RMT_CLK_SRC_DEFAULT;
-    rmt_config.resolution_hz          = kLedStripRmtResHz;
+  led_strip_rmt_config_t rmt_config = {};
+  rmt_config.clk_src = RMT_CLK_SRC_DEFAULT;
+  rmt_config.resolution_hz = kLedStripRmtResHz;
 
-    led_strip_handle_t led_strip;
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-    ESP_LOGI(kTag, "Created LED strip object with RMT backend");
+  led_strip_handle_t led_strip;
+  ESP_ERROR_CHECK(
+      led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+  ESP_LOGI(kTag, "Created LED strip object with RMT backend");
 
-    int64_t current_time = esp_timer_get_time();
-    StepperMotor4P minute_stepper_motor(
-        kMotorStepNumber, kMinuteMotorPin1, kMinuteMotorPin3, kMinuteMotorPin2, kMinuteMotorPin4);
-    ClockWheel minute_clock_wheel(kMinuteSwitchUpPin,
-                                  kMinuteSwitchDownPin,
-                                  kMotorMinuteFrequency,
-                                  minute_stepper_motor,
-                                  current_time,
-                                  led_strip);
+  int64_t current_time = esp_timer_get_time();
 
-    while (true) {
-        current_time = esp_timer_get_time();
-        minute_clock_wheel.Update(current_time);
-        vTaskDelay(pdMS_TO_TICKS(kLoopDelayMs));  // attendre 12ms
-    }
+  StepperMotor4P minute_stepper_motor(kMotorStepNumber, kMinuteMotorPin1,
+                                      kMinuteMotorPin3, kMinuteMotorPin2,
+                                      kMinuteMotorPin4);
+  ClockWheel minute_clock_wheel(kMinuteSwitchUpPin, kMinuteSwitchDownPin,
+                                kMotorMinuteFrequency, minute_stepper_motor,
+                                current_time, led_strip);
+
+  StepperMotor4P hour_stepper_motor(kMotorStepNumber, kHourMotorPin1,
+                                    kHourMotorPin3, kHourMotorPin2,
+                                    kHourMotorPin4);
+  ClockWheel hour_clock_wheel(kHourSwitchUpPin, kHourSwitchDownPin,
+                              kMotorHourFrequency, hour_stepper_motor,
+                              current_time);
+
+  StepperMotor4P day_stepper_motor(kMotorStepNumber, kDayMotorPin1,
+                                   kDayMotorPin3, kDayMotorPin2, kDayMotorPin4);
+  ClockWheel day_clock_wheel(kDaySwitchUpPin, kDaySwitchDownPin,
+                             kMotorDayFrequency, day_stepper_motor,
+                             current_time);
+
+  while (true) {
+    current_time = esp_timer_get_time();
+    minute_clock_wheel.Update(current_time);
+    hour_clock_wheel.Update(current_time);
+    day_clock_wheel.Update(current_time);
+    vTaskDelay(pdMS_TO_TICKS(kLoopDelayMs)); // attendre 12ms
+  }
 }
